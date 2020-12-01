a915e75d0906fd6af72d5d4cbaa4e4147b099af6
Fernando Silva <fernnlcs@gmail.com>
2020-11-30 22:00:01 -0300

    Pontapé inicial do pacote DAO

---
+++ model/DAO/BaseInterDAO.java
@@ -0,0 +1,15 @@
+package model.DAO;
+
+import java.sql.ResultSet;
+
+public interface BaseInterDAO<VO> {
+    public void cadastrar(VO vo);
+
+    public ResultSet listar();
+
+    public ResultSet buscar(VO vo);
+
+    public void editar(VO vo);
+
+    public void excluir(VO vo);
+}
