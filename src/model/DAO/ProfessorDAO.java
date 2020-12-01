a662ae43b6ad007e9bd82e61fdd776ad1cb830e1
Fernando Silva <fernnlcs@gmail.com>
2020-12-01 03:22:42 -0300

    Implementação do método listar() de ProfessorDAO

--- src/model/DAO/ProfessorDAO.java
+++ src/model/DAO/ProfessorDAO.java
@@ -1,7 +1,10 @@
 package src.model.DAO;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 
 import src.model.VO.ProfessorVO;
 
@@ -9,18 +12,38 @@ public class ProfessorDAO extends BaseDAO<ProfessorVO> {
 
     @Override
     public void cadastrar(ProfessorVO vo) {
-        conn = getConnection();
+        Connection conn = getConnection();
         String sql = "insert into professor (nome, email, senha, data_criacao) values (?, ?, ?, ?)";
-        PreparedStatement statement = conn.prepareStatement(sql);
-        statement.setString(1, vo.getNome());
-        statement.setString(2, vo.getEmail());
-        statement.setString(3, vo.getSenha());
-        statement.setString(4, vo.getDataCriacao());
+        PreparedStatement statement;
+
+        try {
+            statement = conn.prepareStatement(sql);
+            statement.setString(1, vo.getNome());
+            statement.setString(2, vo.getEmail());
+            statement.setString(3, vo.getSenha());
+            statement.setTimestamp(4, new Timestamp(vo.getDataCriacao().getTimeInMillis()));
+            statement.execute();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
     public ResultSet listar() {
-        // TODO Auto-generated method stub
+        Connection conn = getConnection();
+        String sql = "select * from professor";
+        PreparedStatement statement;
+
+        try {
+            statement = conn.prepareStatement(sql);
+            statement.execute();
+            return statement.getResultSet();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         return null;
     }
 
@@ -41,5 +64,5 @@ public class ProfessorDAO extends BaseDAO<ProfessorVO> {
         // TODO Auto-generated method stub
 
     }
-    
+
 }
