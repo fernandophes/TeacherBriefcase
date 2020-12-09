2638fb2d9cdd499e0beb3fdeee5e9c29b55a33be
Fernando Silva <fernnlcs@gmail.com>
2020-12-08 23:52:39 -0300

    Criação do FrontCrontroller

--- src/controller/FrontController.java
+++ src/controller/FrontController.java
@@ -1,5 +1,34 @@
 package src.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import src.exception.AuthenticationException;
+import src.model.BO.ProfessorBO;
+import src.model.VO.ProfessorVO;
+
 public class FrontController {
-    
+
+    ProfessorBO professorBO = new ProfessorBO();
+
+    @FXML
+    private Label erroAut;
+    @FXML
+    private TextField loginEmail;
+    @FXML
+    private TextField loginSenha;
+
+    public void autenticar(ActionEvent event) {
+        try {
+            ProfessorVO professor = new ProfessorVO();
+            professor.setEmail(loginEmail.getText());
+            professor.setSenha(loginSenha.getText());
+            professorBO.autenticar(professor);
+        } catch (AuthenticationException e) {
+            erroAut.setText(e.getMessage());
+            erroAut.setVisible(true);
+            e.printStackTrace();
+        }
+    }
 }
