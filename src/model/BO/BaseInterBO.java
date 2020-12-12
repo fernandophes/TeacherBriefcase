b2ee9cc6ae083923674e9cd6c339e0b30465f88d
Fernando Silva <fernnlcs@gmail.com>
2020-12-12 17:03:10 -0300

    Relançar exceção

--- src/model/BO/BaseInterBO.java
+++ src/model/BO/BaseInterBO.java
@@ -6,7 +6,7 @@ public abstract interface BaseInterBO<VO> {
     
     public void cadastrar(VO vo);
 
-    public List<VO> listar();
+    public List<VO> listar() throws Exception;
 
     public VO buscar(VO vo) throws Exception;
 
